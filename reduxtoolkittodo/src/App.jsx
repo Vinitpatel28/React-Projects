573f0929dc0b86e7c6460ae940175fd05cc0d29b
Vinitpatel28 <vinitpatelrajeshbhai@gmail.com>
2026-02-27 17:34:51 +0530

    Add reduxtoolkittodo project

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import './App.css'
+import Todos from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Learn about reduxtoolkit</h1>
+     <AddTodo />
+     <Todos />
+     
+    </>
+  )
+}
+
+export default App
